ffb4c7abf8661da8b8e7505d8ec14dc55d0de34f
swill <swilltec@gmail.com>
2021-05-06 23:48:34 +0000

    refactor: add user filter to state mapStateToProps userheader

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -8,15 +8,15 @@ class UserHeader extends React.Component {
   }
 
   render() {
-    const user = this.props.users.find((user) => user.id === this.props.userId);
-    if (!user){
-        return null
+    const { user } = this.props;
+    if (!user) {
+      return null;
     }
-    return <div>{user.name}</div>;
+    return <div className="header">{user.name}</div>;
   }
 }
 
-const mapStateToProps = (state) => {
-  return { users: state.users };
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find((user) => user.id === ownProps.userId) };
 };
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
